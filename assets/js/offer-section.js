1f82754220779190096e029662e68540af792d19
Marek Samofal <marek.samofal@gmail.com>
2024-01-18 11:51:38 +0100

    setting the function for smooth page scrolling|

--- assets/js/offer-section.js
+++ assets/js/offer-section.js
@@ -1,40 +1,43 @@
 class JobOffersAnimation {
-  constructor() {
-      this.jobOffersRight = document.querySelector('.job-offers__right--event');
-      this.line = document.querySelector('.job-offers__line');
-      this.arrowIcon = document.querySelector('.job-offers__icon');
-      this.offerLink = document.querySelector('.job-offers__link');
+	constructor() {
+		this.jobOffersRight = document.querySelector('.job-offers__right--event')
+		this.line = document.querySelector('.job-offers__line')
+		this.arrowIcon = document.querySelector('.job-offers__icon')
+		this.offerLink = document.querySelector('.job-offers__link')
 
-      this.handleMouseEnter = this.handleMouseEnter.bind(this);
-      this.handleMouseLeave = this.handleMouseLeave.bind(this);
+		this.handleMouseEnter = this.handleMouseEnter.bind(this)
+		this.handleMouseLeave = this.handleMouseLeave.bind(this)
 
-      this.jobOffersRight.addEventListener('mouseenter', this.handleMouseEnter);
-      this.jobOffersRight.addEventListener('mouseleave', this.handleMouseLeave);
-  }
+		this.jobOffersRight.addEventListener('mouseenter', this.handleMouseEnter)
+		this.jobOffersRight.addEventListener('mouseleave', this.handleMouseLeave)
+	}
 
-  handleMouseEnter() {
-      this.line.style.backgroundColor = '#944385';
-      this.line.style.height = '40px';
-      this.line.style.top = '18px';
+	handleMouseEnter() {
+		this.line.style.backgroundColor = '#944385'
+		this.line.style.height = '40px'
+		this.line.style.top = '18px'
 
-      this.arrowIcon.style.filter = 'none';
-      this.arrowIcon.style.top = '48px';
+		this.arrowIcon.style.filter = 'none'
+		this.arrowIcon.style.top = '48px'
 
-      this.offerLink.style.color = 'purple';
-  }
+		this.offerLink.style.color = 'purple'
+	}
 
-  handleMouseLeave() {
-      this.line.style.backgroundColor = '';
-      this.line.style.height = '';
-      this.line.style.top = '';
+	handleMouseLeave() {
+		this.line.style.backgroundColor = ''
+		this.line.style.height = ''
+		this.line.style.top = ''
 
-      this.arrowIcon.style.filter = '';
-      this.arrowIcon.style.top = '';
+		this.arrowIcon.style.filter = ''
+		this.arrowIcon.style.top = ''
 
-      this.offerLink.style.color = '';
-  }
+		this.offerLink.style.color = ''
+	}
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  const jobOffersAnimation = new JobOffersAnimation();
-});
+document.addEventListener('DOMContentLoaded', function () {
+	const jobOffersAnimation = new JobOffersAnimation()
+})
+
+
+
